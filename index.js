d765b2dbdd0d4443c580b4fdb15676b5c06c270e
Matthew Williams <matthewwilliams@matts-mbp.home>
2022-01-11 14:00:44 +0000

    Populated role classes

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
+const Engineer = require("./lib/engineer");
+
+const team = [];
+
+const init = () => {};
