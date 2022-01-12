7bbd3d1b475537405c81b28e39998db7d18e19b7
Matthew Williams <matthewwilliams@matts-mbp.home>
2022-01-12 18:36:29 +0000

    Added prompts for team name and manager info

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
 const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
 const Engineer = require("./lib/engineer");
 
 const team = [];
 
-const init = () => {};
+const startApplication = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "teamName",
+        message:
+          "Welcome to the team profile generator. Please enter the name of your team:",
+      },
+    ])
+    .then((answer) => {
+      const teamName = answer.teamName;
+      getManager();
+    });
+};
+
+const getManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the name of the team manager?",
+      },
+      {
+        type: "input",
+        name: "managerID",
+        message: "What is the ID number of the team manager?",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the email address of the team manager?",
+      },
+      {
+        type: "input",
+        name: "managerNumber",
+        message: "What is the phone number of the team manager?",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager();
+      manager.name = answers.managerName;
+      manager.email = answers.managerEmail;
+      manager.id = answers.managerID;
+      manager.phoneNumber = answers.managerNumber;
+      team.push(manager);
+      console.log(team);
+    });
+};
+
+startApplication();
